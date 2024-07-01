67ea3c435ab7a75d3678a20e6d44536d34d6ee40
NecrCode <90788311+NecrCode@users.noreply.github.com>
2024-07-01 04:04:15 +0000

    Update DisplayBaseMentalState.cs

--- source/iguana_acs_functions/DisplayBaseMentalState.cs
+++ source/iguana_acs_functions/DisplayBaseMentalState.cs
@@ -11,13 +11,13 @@ namespace iguana_acs_functions
     {
         public static bool enabled = true;
         //private static GImage baseMentalStateBar = null;
-        public static GImage initializeUIProgressBar(UI_Panel_ThingInfo Panel)
+        public static GImage initializeUIProgressBar( UI_Panel_ThingInfo Panel )
         {
-            var baseMentalStateBar = UIPackage.CreateObjectFromURL("ui://0xrxw6g7nhdq52ow1z") as GImage;
+            var baseMentalStateBar = UIPackage.CreateObjectFromURL( "ui://0xrxw6g7nhdq52ow1z" ) as GImage;
             UI_NormalSlider currentMentalStateSlider = Panel.m_n39;
             baseMentalStateBar.name = "baseMentalStateBar";
             baseMentalStateBar.height = currentMentalStateSlider.m_bar.height;
-            baseMentalStateBar.color = new Color(0.5f, 1, 1, 1);
+            baseMentalStateBar.color = new Color( 0.5f, 1, 1, 1 );
             baseMentalStateBar.x = currentMentalStateSlider.m_bar.x;
             baseMentalStateBar.y = currentMentalStateSlider.m_bar.y;
             return baseMentalStateBar;
@@ -28,18 +28,25 @@ namespace iguana_acs_functions
             //baseMentalStateBar = null;
         }
 
-        [HarmonyPatch(typeof(Panel_ThingInfo), "ShowNpc")]
+        [HarmonyPatch( typeof( Panel_ThingInfo ), "ShowNpc" )]
         public class UpdateBaseMentalStateSlider
         {
-            static void Postfix(Npc npc, UI_Panel_ThingInfo ___Panel)
+            static void Postfix( Npc npc, UI_Panel_ThingInfo ___Panel )
             {
                 try
                 {
                     if ( !enabled ) { return; }
                     if ( npc == null || ___Panel == null ) { return; }
-                    if ( !npc.IsPlayerThing || npc.IsVistor || npc.GongKind != g_emGongKind.Dao ) { return; }
-                    
-                    
+                    if ( !npc.IsPlayerThing || npc.IsVistor || npc.GongKind != g_emGongKind.Dao )
+                    {
+                        if( ___Panel.m_n39.GetChild( "baseMentalStateBar" ) != null )
+                        {
+                            ___Panel.m_n39.GetChild( "baseMentalStateBar" ).visible = false;
+                        }
+                        return;
+                    }
+
+
                     string mentalStateDisplayName = PropertyMgr.Instance.GetDef( "MindStateBaseValue" ).DisplayName;
                     float baseMentalState = npc.GetPropertySecond( "MindStateBaseValue" );
                     int needLevel = npc.Needs.GetNeedLevel( g_emNeedType.MindState );
@@ -55,18 +62,25 @@ namespace iguana_acs_functions
                     }
                     ___Panel.m_n39.tooltips = string.Format( TFMgr.Get( "心境:{0:N0} ({1}){2}\n{3}: {4:N0}\n心境会大幅度影响修炼相关行为的效率和成功率。" ), ___Panel.m_n39.value, GameDefine.MindState2Txt[needLevel], mentalStateExtraText, mentalStateDisplayName, baseMentalState );
                     UI_NormalSlider currentMentalStateSlider = ___Panel.m_n39;
+                    var baseMentalStateBar = currentMentalStateSlider.GetChild( "baseMentalStateBar" );
+
+                    if ( baseMentalStateBar != null )
+                    {
+                        baseMentalStateBar.visible = true;
+                    }
+
                     if ( currentMentalStateSlider.numChildren == 9 )
                     {
-                        var baseMentalStateBar  = initializeUIProgressBar( ___Panel );
+                        baseMentalStateBar = initializeUIProgressBar( ___Panel );
                         currentMentalStateSlider.AddChild( baseMentalStateBar );
                     }
                     if ( baseMentalState < 0 )
                     {
-                        currentMentalStateSlider.GetChild( "baseMentalStateBar" ).width = 0;
+                        baseMentalStateBar.width = 0;
                     }
                     else
                     {
-                        currentMentalStateSlider.GetChild( "baseMentalStateBar" ).width = Math.Min( 1, baseMentalState / 100 ) * currentMentalStateSlider.width;
+                        baseMentalStateBar.width = Math.Min( 1, baseMentalState / 100 ) * currentMentalStateSlider.width;
                     }
                 }
                 catch ( Exception e )
